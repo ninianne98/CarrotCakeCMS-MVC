93cf3f4766fc5de754ba5c43aea24addf0e706db
samantha copeland <ninianne98@gmail.com>
2024-04-07 17:51:10 -0500

    jquery & mvc updates

--- WebComponents/jquerybasic.cs
+++ WebComponents/jquerybasic.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text;
-using System.Web;
 
 /*
 * CarrotCake CMS (MVC5)
@@ -13,82 +12,81 @@ using System.Web;
 
 namespace Carrotware.Web.UI.Components {
 
-	public class jquerybasic : BaseWebComponent {
+    public class jquerybasic : BaseWebComponent {
 
-		public jquerybasic() {
-			this.JQVersion = jquery.DefaultJQVersion;
-			this.UseJqueryMigrate = false;
-			this.StylesheetOnly = false;
-			this.SelectedSkin = jQueryTheme.GlossyBlack;
-		}
+        public jquerybasic() {
+            this.JQVersion = jquery.DefaultJQVersion;
+            this.UseJqueryMigrate = this.JQVersion.StartsWith("3") ? true : false;
+            this.StylesheetOnly = false;
+            this.SelectedSkin = jQueryTheme.GlossyBlack;
+        }
 
-		public enum jQueryTheme {
-			GlossyBlack,
-			Silver,
-			Purple,
-			Blue,
-			Green,
-			LightGreen,
-			NotUsed
-		}
+        public enum jQueryTheme {
+            GlossyBlack,
+            Silver,
+            Purple,
+            Blue,
+            Green,
+            LightGreen,
+            NotUsed
+        }
 
-		public jQueryTheme SelectedSkin { get; set; }
+        public jQueryTheme SelectedSkin { get; set; }
 
-		public bool StylesheetOnly { get; set; }
+        public bool StylesheetOnly { get; set; }
 
-		public string JQVersion { get; set; }
+        public string JQVersion { get; set; }
 
-		public bool UseJqueryMigrate { get; set; }
+        public bool UseJqueryMigrate { get; set; }
 
+        private jquery _jq = new jquery();
+        private jqueryui _jqui = new jqueryui();
 
-		private jquery _jq = new jquery();
-		private jqueryui _jqui = new jqueryui();
+        public override string GetHtml() {
+            string sJQFile = string.Empty;
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(string.Empty);
 
-		public override string GetHtml() {
-			string sJQFile = string.Empty;
-			StringBuilder sb = new StringBuilder();
-			sb.AppendLine(string.Empty);
+            if (!this.StylesheetOnly) {
+                _jq.JQVersion = this.JQVersion;
+                _jq.UseJqueryMigrate = this.UseJqueryMigrate;
 
-			if (!this.StylesheetOnly) {
-				_jq.JQVersion = this.JQVersion;
-				_jq.UseJqueryMigrate = this.UseJqueryMigrate;
+                sb.AppendLine(_jq.GetHtml().Trim());
+                sb.AppendLine(_jqui.GetHtml().Trim());
+            }
 
-				sb.AppendLine(_jq.GetHtml().Trim());
-				sb.AppendLine(_jqui.GetHtml().Trim());
-			}
+            switch (this.SelectedSkin) {
+                case jQueryTheme.GlossyBlack:
+                    sJQFile = CarrotWeb.GetWebResourceUrl("jquerybasic.jquery-ui-black.css");
+                    break;
 
-			switch (this.SelectedSkin) {
-				case jQueryTheme.GlossyBlack:
-					sJQFile = CarrotWeb.GetWebResourceUrl("jquerybasic.jquery-ui-black.css");
-					break;
+                case jQueryTheme.Purple:
+                    sJQFile = CarrotWeb.GetWebResourceUrl("jquerybasic.jquery-ui-purple.css");
+                    break;
 
-				case jQueryTheme.Purple:
-					sJQFile = CarrotWeb.GetWebResourceUrl("jquerybasic.jquery-ui-purple.css");
-					break;
+                case jQueryTheme.Green:
+                    sJQFile = CarrotWeb.GetWebResourceUrl("jquerybasic.jquery-ui-green.css");
+                    break;
 
-				case jQueryTheme.Green:
-					sJQFile = CarrotWeb.GetWebResourceUrl("jquerybasic.jquery-ui-green.css");
-					break;
+                case jQueryTheme.Blue:
+                    sJQFile = CarrotWeb.GetWebResourceUrl("jquerybasic.jquery-ui-blue.css");
+                    break;
 
-				case jQueryTheme.Blue:
-					sJQFile = CarrotWeb.GetWebResourceUrl("jquerybasic.jquery-ui-blue.css");
-					break;
+                case jQueryTheme.LightGreen:
+                    sJQFile = CarrotWeb.GetWebResourceUrl("jquerybasic.jquery-ui-lightgreen.css");
+                    break;
 
-				case jQueryTheme.LightGreen:
-					sJQFile = CarrotWeb.GetWebResourceUrl("jquerybasic.jquery-ui-lightgreen.css");
-					break;
+                case jQueryTheme.Silver:
+                default:
+                    sJQFile = CarrotWeb.GetWebResourceUrl("jquerybasic.jquery-ui-silver.css");
+                    break;
+            }
 
-				case jQueryTheme.Silver:
-				default:
-					sJQFile = CarrotWeb.GetWebResourceUrl("jquerybasic.jquery-ui-silver.css");
-					break;
-			}
+            if (this.SelectedSkin != jQueryTheme.NotUsed) {
+                sb.AppendLine(UrlPaths.CreateCssTag(string.Format("JQuery UI CSS {0}", this.SelectedSkin), sJQFile));
+            }
 
-			if (this.SelectedSkin != jQueryTheme.NotUsed) {
-				sb.AppendLine(UrlPaths.CreateCssTag(string.Format("JQuery UI CSS {0}", this.SelectedSkin), sJQFile));
-			}
-
-			return sb.ToString().Trim();
-		}
-	}
+            return sb.ToString().Trim();
+        }
+    }
 }
